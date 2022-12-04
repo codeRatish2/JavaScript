8fe4f4aec33ce643f7a5cf24a627c849b868feb3
Ratiesh Dalvi <115468621+codeRatish2@users.noreply.github.com>
2022-12-04 17:56:52 +0530

    Add files via upload

---
+++ key, value.js
@@ -0,0 +1,21 @@
+var superman = {
+  name: "superman",
+  power: "flight",
+  costumecolor: "blue",
+  strength: "10000",
+  stelth: "0",
+}
+var batman = {
+  name: "batman",
+  power: "martial arts",
+  costumecolor: "black",
+  strength: "100",
+  stelth: "100",
+}
+console.log(superman.strength);
+console.log(batman.strength);
+console.log(superman.strength > batman.strength);
+
+console.log(superman.stelth);
+console.log(batman.stelth);
+console.log(superman.stelth < batman.stelth)
