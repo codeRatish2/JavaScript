8cb9b2666c70096c40e31bf9f066ee2d19ae0f2d
Ratiesh Dalvi <115468621+codeRatish2@users.noreply.github.com>
2022-12-06 18:13:41 +0530

    Add files via upload

---
+++ function myfunction(num1, num2){.js
@@ -0,0 +1,8 @@
+function myfunction(num1,num2){
+    console.log(num1+num2 );
+
+}
+
+
+myfunction(1000,345);
+
---
+++ variable declaration-1.js
@@ -0,0 +1,9 @@
+// variable declaration 
+
+var user = 'Ratish ' age = '26' message = 'hello'
+
+// 
+var user = 'ratish';
+    age = '26';
+    message = 'hello';
+                  
